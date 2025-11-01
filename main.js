aae8208fcddc757e2e29dbbf6b22d4e1c05331bc
vicky-voin <alteneria127@gmail.com>
2025-11-01 11:50:55 -0400

    Set up ejs and a test view

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 require("dotenv").config();
+const path = require("path");
 const express = require("express");
 const app = express();
 const db = require("./db/queries");
 
+const assetPath = path.join(__dirname, "public");
+
+app.use(express.static(assetPath));
+app.use(express.urlencoded({ extended: true }));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.get("/", async (req, res) => {
   const items = await db.testQuery();
-  res.send("Items: " + items.map((item) => item.name).join(", "));
+  res.render("home", { items: items.map((item) => item.name) });
 });
 
 const PORT = process.env.PORT || 3000;
